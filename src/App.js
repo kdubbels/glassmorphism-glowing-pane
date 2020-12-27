36786a3f4a8fffa946b13b739ca18868ba2098fa
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2020-12-27 14:21:12 -0700

    add dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useCallback, useRef } from 'react';
+import { useEffect, useReducer, useCallback, useRef, useState } from 'react';
 import './App.css';
 
 const SET_TIME = 'SET_TIME';
@@ -40,6 +40,17 @@ function reducer(state, action) {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode() {
+    console.log(darkMode);
+    if (darkMode) {
+      document.querySelector('body').style.background = 'white';
+    } else {
+      document.querySelector('body').style.background = '#212';
+    }
+    setDarkMode(!darkMode);
+  }
 
   function setTime({hour, minutes, seconds}) {
     dispatch({
@@ -74,9 +85,15 @@ function App() {
   const { hour, minutes, seconds } = state;
 
   return (
-    <div className="App">
-      <div className={`display`}>
+    <div className={`App ${darkMode ? '' : `light-mode`}`}>
+      <div className={`display ${darkMode ? '' : `light-mode`}`}>
         <h1 className={`display__time`}>{hour < 10 ? `0${hour}` : hour}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+      </div>
+      <div>
+        <label className="switch">
+          <input type="checkbox" onChange={() => toggleDarkMode()}/>
+          <span className="slider round"></span>
+        </label>
       </div>
     </div>
   );
