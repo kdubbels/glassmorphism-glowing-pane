c1605e4291e69a23a596b0c7a8448464eacc9d56
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2020-12-26 20:11:55 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import { useEffect, useReducer, useCallback, useRef } from 'react';
 import './App.css';
 
+const SET_TIME = 'SET_TIME';
+
+// useInterval hook from 30secondsofcode
+// https://www.30secondsofcode.org/react/s/use-interval
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
+
+const initialState = {
+  hour: undefined, 
+  minutes: undefined, 
+  seconds: undefined
+}
+
+function reducer(state, action) {
+  if (action.type === SET_TIME) {
+    return {
+      ...state,
+      ...action.payload
+    };
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function setTime({hour, minutes, seconds}) {
+    dispatch({
+      type: SET_TIME,
+      payload:{
+        hour,
+        minutes,
+        seconds
+      }
+    })
+  }
+
+  const setDate = useCallback(
+    () => {
+      const now = new Date();
+
+      const seconds = now.getSeconds();
+
+      const minutes = now.getMinutes();
+
+      const hour = now.getHours();
+
+      setTime({hour, minutes, seconds});
+    },
+    [],
+  );
+
+  useInterval(() => {
+    setDate();
+  }, 100);
+  
+  const { hour, minutes, seconds } = state;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`display`}>
+        <h1 className={`display__time`}>{hour < 10 ? `0${hour}` : hour}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+      </div>
     </div>
   );
 }
